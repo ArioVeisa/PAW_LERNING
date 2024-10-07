2128882d8be4fd906af8cdeb39032f640d8951b7
arioveisa <arioveisa@gmail.com>
2024-10-07 06:54:21 +0000

    add a navbar

---
+++ modul_3/script.js
@@ -0,0 +1,6 @@
+// Hamburger menu toggle
+const hamburger = document.getElementById('hamburger');
+const menu = document.getElementById('menu');
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+});
